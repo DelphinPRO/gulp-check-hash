7078209c7abc5c67306a23ca60ffc264e3a56296
DelphinPRO <delphinpro@yandex.ru>
2015-06-23 18:06:42 +0400

    add config option "type (hash|filetime)"

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ var crypto = require('crypto');
 var defaults = require('lodash.defaults');
 var through = require('through2');
 
-//function isTest(file) {
-//	var testFile = '';
-//	return file.path == testFile;
-//}
-
 var plugin = function (opt) {
 	var opts = {
 		src      : 'src',
 		build    : 'build',
-		extension: null
+		extension: null,
+		type     : 'hash' // hash | filetime
 	};
 	opts = defaults(opt || {}, opts);
 
@@ -23,13 +19,8 @@ var plugin = function (opt) {
 	var stream = through.obj(function (file, enc, callback) {
 		var contents = null;
 		var isModified = true;
+		var srcStats = null;
 		var stats = null;
-
-		//if (isTest(file)) {
-		//	console.log('ROOT DIR  : ' + plugin.root);
-		//	console.log('SRC DIR   : ' + srcDirectory);
-		//	console.log('BUILD DIR : ' + buildDirectory);
-		//}
 
 		if (file.isStream()) {
 			this.push(file);
@@ -37,54 +28,38 @@ var plugin = function (opt) {
 		}
 
 		if (file.isBuffer()) {
-			contents = file.contents.toString('utf8');
-			contents = crypto.createHash('md5').update(contents).digest('hex');
-
 			var extName = path.extname(file.relative);
 			var baseName = path.basename(file.relative, extName);
 			var pathName = path.dirname(file.relative);
 			var destFile = path.join(buildDirectory + '/' + pathName + '/' + baseName + (opts.extension ? opts.extension : extName));
-
-
-			//if (isTest(file)) {
-			//	console.log('relative : ' + file.relative);
-			//	console.log('pathname : ' + pathName);
-			//	console.log('basename : ' + baseName + extName);
-			//	console.log('destFile   : ' + destFile);
-			//}
-
-
 			if (fs.existsSync(destFile)) {
 				stats = fs.lstatSync(destFile);
+
+				if (opts.type == 'filetime') {
+					srcStats = fs.lstatSync(path.join(srcDirectory + '/' + file.relative));
+					if ((new Date(srcStats.mtime)).getTime() <= (new Date(stats.mtime)).getTime()) {
+						isModified = false;
+					}
+				}
 			}
+			if (isModified && (opts.type == 'hash')) {
+				contents = file.contents.toString('utf8');
+				contents = crypto.createHash('md5').update(contents).digest('hex');
 
-			if (stats && stats.isFile()) {
-				var destContents = fs.readFileSync(destFile, 'utf-8');
-				var destContentsHash = crypto.createHash('md5').update(destContents).digest('hex');
+				if (stats && stats.isFile()) {
+					var destContents = fs.readFileSync(destFile, 'utf-8');
+					var destContentsHash = crypto.createHash('md5').update(destContents).digest('hex');
 
-				//if (isTest(file)) {
-				//	console.log('FILE         : ' + file.path);
-				//	console.log('BUFFER       : ' + file.path);
-				//	console.log('RELATIVE     : ' + file.relative);
-				//	console.log('EXTNAME      : ' + extName);
-				//	console.log('BASENAME     : ' + baseName);
-				//	console.log('DEST         : ' + buildDirectory);
-				//	console.log('DEST FILE    : ' + destFile);
-				//	console.log('HASH SRC     : ' + contents);
-				//	console.log('HASH DEST    : ' + destContentsHash);
-				//}
-
-				if (contents === destContentsHash) {
-					isModified = false;
+					if (contents === destContentsHash) {
+						isModified = false;
+					}
 				}
 			}
 		}
-
 		if (!isModified) {
 			callback();
 			return;
 		}
-
 		console.log('check hash: changed (' + file.relative + ')');
 		this.push(file);
 		callback();
